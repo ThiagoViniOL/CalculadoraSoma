50505ae8236d320ed1b94a583780e8c5585e7e0a
Thiago Lima <thiago.vl00@outlook.com>
2024-11-21 10:37:07 -0300

    criação de um programa q calcula a soma de 2 valores

---
+++ CalculadoraSoma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraSoma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor de x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            int soma = x + y;
+
+            Console.WriteLine(soma);
+            Console.ReadKey();
+
+        }
+    }
+}
